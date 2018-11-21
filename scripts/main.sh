25a6325e9d89e340965db92b3174ed98bf2c9001
Julia Beliaeva <Julia.Beliaeva@jetbrains.com>
2018-11-22 01:44:27 +0300

    add arguments checks

--- scripts/main.sh
+++ scripts/main.sh
@@ -1,14 +1,38 @@
 #!/bin/bash
 
+if [ "$#" -ne 2 ]
+then
+	echo "Usage: $0 <REPOSITORY> <PATH IN REPOSITORY>" >&2
+	exit 1
+fi
+
+if ! [ -e "$1" ] || ! [ -d "$1" ]
+then
+	echo "$1 is not found or is not a directory" >&2
+	exit 1
+fi
+
+if ! [ -e "$1/$2" ]
+then
+	echo "$2 is not found in repository $1" >&2
+	exit 1
+fi
+
 script=`realpath $0`
 scriptdir=`dirname $script`
 tmpdir=`realpath $PWD`/generated
 mkdir -p "$tmpdir"
 
-repository=$1
-target=$2
+repository="$1"
+target="$2"
 
 pushd "$repository"
+
+if ! [ -e ".git" ]
+then
+	echo "$1 is not a git repository" >&2
+	exit 1
+fi
 
 for file in $(find "$target" -depth -name '*')
 do
